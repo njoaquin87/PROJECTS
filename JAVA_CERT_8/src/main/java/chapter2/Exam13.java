e7af53a2645c1192732e57b391d32e9d39dbaa58
njoaquin87 <nilton.joaquin@gmail.com>
2016-05-15 14:35:52 -0500

    Exam13 Chapter 2

---
+++ JAVA_CERT_8/src/main/java/chapter2/Exam13.java
@@ -0,0 +1,15 @@
+package chapter2;
+/********
+ * book: OCA Oracle Certified Associate JAVA SE 8 (2014)
+ * Chapter 2: Operator and Statements
+
+ * ****/
+public class Exam13 {
+
+	public static void main(String args[]){
+		int x1 = 50, x2 = 75;
+		boolean b = x1 >= x2;
+		if(b=true) System.out.println("Success");
+		else System.out.println("Failure");
+	}
+}
