8f0edfc693301f771a63782707d64b4c09c5afef
Nilton Joaquin <njoaquinf@proveedor.intercorp.com.pe>
2017-03-29 12:17:55 -0500

    Angular Example

---
+++ parent/frontend/src/main/frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ng2-bootstrap';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AlertModule.forRoot(),
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
